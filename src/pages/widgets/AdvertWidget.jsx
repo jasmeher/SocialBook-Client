da1b743b88b532c25ad79ba2960635d13ad7d468
Jasmeher <bharadwajjasmeher@gmail.com>
2023-01-16 21:48:23 +0530

    updated fetch links

--- src/pages/widgets/AdvertWidget.jsx
+++ src/pages/widgets/AdvertWidget.jsx
@@ -29,7 +29,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/advert.png"
+        src="https://socialbook-api-xwvp.onrender.com/assets/teckwear.png"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
